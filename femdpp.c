61e90b2069422fcb6495e1afb92bd0c229fb1d70
Jon Ringuette <bobskr@gmail.com>
2021-03-31 14:16:08 -0700

    Updated to no longer force pulser

--- femdpp.c
+++ femdpp.c
@@ -16,13 +16,13 @@ BOOL frontend_call_loop  = FALSE; /* call frontend_loop periodically if TRUE */
 INT display_period       = 0;   /* Frontend status page update interval [ms] */
 INT max_event_size       = 524288;      /* max event size from this frontend */
 INT max_single_event_size     = 47369;
-INT max_event_size_frag  = 5 * 1024*1024; /* max event fragmented event size */
+INT max_event_size_frag  = 5 * 1024 * 1024; /* max event fragmented event size */
 INT event_buffer_size = 4 * 800000;            /* buffer size to hold events */
 
 #define VME_DMA_BUFSIZE 0x80000   // 0.5MByte
 #define EVT_BUFSIZE     0x800000  // 8MByte - we don't check for overwriting
-                                  //  this buffer - so make large enough
-                                  // for several DMA buffers of data
+//  this buffer - so make large enough
+// for several DMA buffers of data
 #define MDPP16_NUMCHAN 16
 #define MDPP16_BASE     0x00100000   /* set by jumpers on board */
 const int mdpp16_addr = MDPP16_BASE; /* VME base address */
@@ -51,26 +51,27 @@ BANK_LIST trigger_bank_list[] = {
 #undef USE_INT
 EQUIPMENT equipment[] = {
 
-    {"MDPP16",               /* equipment name */
-     {400, 0,                   /* event ID, trigger mask */
-      "SYSTEM",               /* event buffer */
+    {   "MDPP16",               /* equipment name */
+        {   400, 0,                   /* event ID, trigger mask */
+            "SYSTEM",               /* event buffer */
 #ifdef USE_INT
-      EQ_INTERRUPT,           /* equipment type */
+            EQ_INTERRUPT,           /* equipment type */
 #else
-      EQ_POLLED, //PERIODIC, // POLLED,              /* equipment type */
+            EQ_POLLED, //PERIODIC, // POLLED,              /* equipment type */
 #endif
-      LAM_SOURCE(0, 0x0),     /* event source crate 0, all stations */
-      "MIDAS",                /* format */
-      TRUE,                   /* enabled */
-      RO_RUNNING,          /* read only when running */
-      500,                    /* poll for 500ms */
-      0,                      /* stop run after this event limit */
-      0,                      /* number of sub events */
-      0,                      /* don't log history */
-      "", "", "",},
-     read_trigger_event,      /* readout routine */
-     NULL, NULL,
-     trigger_bank_list,
+            LAM_SOURCE(0, 0x0),     /* event source crate 0, all stations */
+            "MIDAS",                /* format */
+            TRUE,                   /* enabled */
+            RO_RUNNING,          /* read only when running */
+            500,                    /* poll for 500ms */
+            0,                      /* stop run after this event limit */
+            0,                      /* number of sub events */
+            0,                      /* don't log history */
+            "", "", "",
+        },
+        read_trigger_event,      /* readout routine */
+        NULL, NULL,
+        trigger_bank_list,
     },
 
 
@@ -79,12 +80,12 @@ EQUIPMENT equipment[] = {
 };
 
 //////////////////////////////////////////////////////////////////////
-INT       frontend_exit (                               ){ return SUCCESS; }
+INT       frontend_exit (                               ) { return SUCCESS; }
 //INT          end_of_run (INT run_number, char *error    ){ return SUCCESS; }
-INT           pause_run (INT run_number, char *error    ){ return SUCCESS; }
-INT          resume_run (INT run_number, char *error    ){ return SUCCESS; }
-INT       frontend_loop (                               ){ return SUCCESS; }
-INT interrupt_configure ( INT cmd, INT source, PTYPE adr){ return SUCCESS; }
+INT           pause_run (INT run_number, char *error    ) { return SUCCESS; }
+INT          resume_run (INT run_number, char *error    ) { return SUCCESS; }
+INT       frontend_loop (                               ) { return SUCCESS; }
+INT interrupt_configure ( INT cmd, INT source, PTYPE adr) { return SUCCESS; }
 
 void seq_callback(INT hDB, INT hseq, void *in)
 {
@@ -114,28 +115,28 @@ INT frontend_init()
 
     sprintf(set_str, "/Equipment/MDPP16/Settings");
     status = db_create_record(hDB, 0, set_str, strcomb(mdpp16_settings_str));
-    if( (status = db_find_key (hDB, 0, set_str, &hSet)) != DB_SUCCESS ){
-     cm_msg(MINFO,"FE","Key %s not found", set_str);
+    if ( (status = db_find_key (hDB, 0, set_str, &hSet)) != DB_SUCCESS ) {
+        cm_msg(MINFO, "FE", "Key %s not found", set_str);
     }
-    if ((status =db_open_record(hDB, hSet, &mdpp16, size, /* enable hot-link */
-    			       MODE_READ, seq_callback, NULL)) != DB_SUCCESS ){
-      cm_msg(MINFO,"FE","Failed to enable ts hotlink", set_str);
+    if ((status = db_open_record(hDB, hSet, &mdpp16, size, /* enable hot-link */
+                                 MODE_READ, seq_callback, NULL)) != DB_SUCCESS ) {
+        cm_msg(MINFO, "FE", "Failed to enable ts hotlink", set_str);
     }
     printf("Got here 2\n");
 
     mvme_open(&vme_handle, 0);
-    if( vme_handle == NULL ){
-	printf("VME init failed\n");
-	return -1;
+    if ( vme_handle == NULL ) {
+        printf("VME init failed\n");
+        return -1;
     }
     hVme = (int)vme_handle;
-    if( (dma_buffer=(unsigned*)malloc(VME_DMA_BUFSIZE*sizeof(int))) == NULL ){
-	printf("VME_DMA_BUF malloc failed\n");
-	return -1;
+    if ( (dma_buffer = (unsigned*)malloc(VME_DMA_BUFSIZE * sizeof(int))) == NULL ) {
+        printf("VME_DMA_BUF malloc failed\n");
+        return -1;
     }
-    if( (evt_buffer=(unsigned*)malloc(EVT_BUFSIZE*sizeof(int))) == NULL ){
-	printf("EVT_BUF malloc failed\n");
-	return -1;
+    if ( (evt_buffer = (unsigned*)malloc(EVT_BUFSIZE * sizeof(int))) == NULL ) {
+        printf("EVT_BUF malloc failed\n");
+        return -1;
     }
 
     mdpp16_reset(hVme, mdpp16_addr);
@@ -151,12 +152,12 @@ INT frontend_init()
 INT poll_event(INT source, INT count, BOOL test)
 {
     int i, words;
-    for(i=0; i<count; i++){
-	if( dma_bufpos ){ words=1; } // still data in buffer
-	else {
-	    words = mdpp16_read_fifo_words(hVme, mdpp16_addr);
-	}
-	if( words && !test ){ return words; }
+    for (i = 0; i < count; i++) {
+        if ( dma_bufpos ) { words = 1; } // still data in buffer
+        else {
+            words = mdpp16_read_fifo_words(hVme, mdpp16_addr);
+        }
+        if ( words && !test ) { return words; }
     }
     return 0;
 }
@@ -164,24 +165,24 @@ INT poll_event(INT source, INT count, BOOL test)
 void dump_event(int *data, int size)
 {
     int i;
-    for(i=0; i<size; i++){
-	printf(" 0x%08x", data[i] );
-	if( ((i+1) % 8 ) == 0 ){ printf("\n    "); }
+    for (i = 0; i < size; i++) {
+        printf(" 0x%08x", data[i] );
+        if ( ((i + 1) % 8 ) == 0 ) { printf("\n    "); }
     }
-    if( (i % 8) != 0 ){ printf("\n"); }
+    if ( (i % 8) != 0 ) { printf("\n"); }
 }
 
 
-static int current_chan=0, bank1_flag = 1, bank_done=1;
-static int readout_counter=0;
+static int current_chan = 0, bank1_flag = 1, bank_done = 1;
+static int readout_counter = 0;
 
 INT read_trigger_event(char *pevent, INT off)
 {
     DWORD *pdata;
     unsigned int i, words = 0;
     unsigned int test = 0;
-    words = mdpp16_read_fifo_words(hVme, mdpp16_addr);
-    printf("Number of words : %i\n", words);
+    words = mdpp16_read_fifo_words(hVme, mdpp16_addr);  // Reads buffer length
+    //printf("Number of words : %i\n", words);
     if ( words <= 0 ) { return (0); }
     bk_init(pevent);                               /* init bank structure */
     bk_create(pevent, "MDPP", TID_DWORD, (void **)&pdata); /* create SCLR bank */
@@ -190,30 +191,30 @@ INT read_trigger_event(char *pevent, INT off)
 
 //    printf("My DATA LEN thinggie!! : %i\n", test);
 //    if ( words > max_single_event_size ) {
-        printf("read_trigger_event: event-size[%d] > limit[%d]\n", words, max_single_event_size);
-        for (i = 0; i <= words+1; i++) {  // Not super sure about the words+1 it mentions you should read data len +1
-            vme_A32D32_read(hVme, mdpp16_addr, pdata++);
+    //printf("read_trigger_event: event-size[%d] : limit[%d]\n", words, max_single_event_size);
+    for (i = 0; i <= words + 1; i++) { // Not super sure about the words+1 it mentions you should read data len +1
+        vme_A32D32_read(hVme, mdpp16_addr, pdata++);
 
-        }
-        printf("Writing reset...");
-        vme_A32D16_write(hVme, mdpp16_addr + MDPP16_READOUT_RESET,  0);
-        printf("Done\n");
-     //   return (0);
-            bk_close(pevent, pdata);                      /* close scaler bank */
+    }
+    //printf("Writing reset...");
+    vme_A32D16_write(hVme, mdpp16_addr + MDPP16_READOUT_RESET,  0);
+    //printf("Done\n");
+    //   return (0);
+    bk_close(pevent, pdata);                      /* close scaler bank */
 
-  //  } else {
+    //  } else {
 
 
     //for (i = 0; i < words; i++) {
-     //   vme_A32D32_read(hVme, mdpp16_addr, pdata++);
+    //   vme_A32D32_read(hVme, mdpp16_addr, pdata++);
     //}
-   // bk_close(pevent, pdata);                      /* close scaler bank */
-   // vme_A32D16_write(hVme, mdpp16_addr + MDPP16_READOUT_RESET,  0);
+    // bk_close(pevent, pdata);                      /* close scaler bank */
+    // vme_A32D16_write(hVme, mdpp16_addr + MDPP16_READOUT_RESET,  0);
 
 //}
     readout_counter += 1;
-    printf("Event: %8d\r", readout_counter);
-    printf("Block Size : %i\n", bk_size(pevent));
+ //   printf("Event: %8d\r", readout_counter);
+  //  printf("Block Size : %i\n", bk_size(pevent));
     return bk_size(pevent);
 }
 
@@ -232,23 +233,23 @@ INT read_scaler_event(char *pevent, INT off)
 
 INT end_of_run(INT run_number, char *error)
 {
-    printf("Begin of EOR %i\n",run_number);
+    printf("Begin of EOR %i\n", run_number);
 
     mdpp16_acq_stop(hVme, mdpp16_addr);
 
-    printf("End of EOR %i\n",run_number);
+    printf("End of EOR %i\n", run_number);
     return SUCCESS;
 }
 
 INT begin_of_run(INT run_number, char *error) {
-    int i,j;
+    int i, j;
 
-    printf("Begin of BOR %i\n",run_number);
+    printf("Begin of BOR %i\n", run_number);
 
     mdpp16_config(hVme, mdpp16_addr);
     mdpp16_acq_start(hVme, mdpp16_addr);
 
-    printf("End of BOR %i\n",run_number);
+    printf("End of BOR %i\n", run_number);
     return SUCCESS;
 }
 
@@ -261,35 +262,35 @@ void print_return(const char* desc, int val)
 void set_wr(const char* desc, unsigned int offset, unsigned int data)
 {
     printf("   setting: %-24s to value: %5d ...", desc, data);
-    int retval = vme_A32D16_write(hVme, mdpp16_addr+offset, data);
+    int retval = vme_A32D16_write(hVme, mdpp16_addr + offset, data);
 
     unsigned int val = 0;
-    vme_A32D16_read(hVme, mdpp16_addr+offset,  &val);
+    vme_A32D16_read(hVme, mdpp16_addr + offset,  &val);
     data != val
-	? printf("NOT succesful; reread value: %d, expected: %d\n", val, data)
-	: printf("successful\n");
+    ? printf("NOT succesful; reread value: %d, expected: %d\n", val, data)
+    : printf("successful\n");
 }
 
 void set_w(const char* desc, unsigned int offset, unsigned int data)
 {
     printf("   setting: %-24s to value: %5d ...", desc, data);
-    vme_A32D16_write(hVme, mdpp16_addr+offset, data);
+    vme_A32D16_write(hVme, mdpp16_addr + offset, data);
     printf("done\n");
 }
 
 int mdpp16_config(int hVme, unsigned mdpp16_addr)
 {
     int version = mdpp16_version(hVme, mdpp16_addr);
-    if( version != -1 ){
-	printf("mdpp16: firmware version=0x%04x\n", version);
+    if ( version != -1 ) {
+        printf("mdpp16: firmware version=0x%04x\n", version);
     } else {
-	printf("mdpp16: not found\n");
-	return(-1);
+        printf("mdpp16: not found\n");
+        return (-1);
     }
     printf("setting MDPP16 values\n");
-    mdpp16.pulser_ctrl = 1;
-    mdpp16.multi_event = 3;
-    mdpp16.max_datalen = 30;
+    // mdpp16.pulser_ctrl = 1;
+    //mdpp16.multi_event = 0;
+    mdpp16.max_datalen = 60;
 
     // in multi-event-modes, [48k]buffer read will terminate(berr)
     //    at first end of event after max_datalen
@@ -300,13 +301,13 @@ int mdpp16_config(int hVme, unsigned mdpp16_addr)
     set_wr("MDPP16_MAX_DATALEN",    MDPP16_MAX_DATALEN,   mdpp16.max_datalen);
     set_wr("MDPP16_WINDOW_START",   MDPP16_WINDOW_START,  mdpp16.window_start);
     set_wr("MDPP16_WINDOW_WIDTH",   MDPP16_WINDOW_WIDTH,  mdpp16.window_width);
-    set_wr("MDPP16_TDC_RESOLUTION", MDPP16_TDC_RESOLUTION,mdpp16.tdc_resln);
-    set_wr("MDPP16_ADC_RESOLUTION", MDPP16_ADC_RESOLUTION,mdpp16.adc_resln);
+    set_wr("MDPP16_TDC_RESOLUTION", MDPP16_TDC_RESOLUTION, mdpp16.tdc_resln);
+    set_wr("MDPP16_ADC_RESOLUTION", MDPP16_ADC_RESOLUTION, mdpp16.adc_resln);
     set_wr("MDPP16_TRIG_SOURCE",    MDPP16_TRIG_SOURCE,   mdpp16.trig_source);
     set_wr("MDPP16_NIM2_INPUT",     MDPP16_NIM2_INPUT,    mdpp16.nim2_input);
 
     printf("setting MDPP16 channel 0 and 1 values ...\n");
-    set_w( "MDPP16_CHANPAIR_SELECT",MDPP16_CHANPAIR_SELECT, 0x4);
+    set_w( "MDPP16_CHANPAIR_SELECT", MDPP16_CHANPAIR_SELECT, 0x4);
 
     set_wr("MDPP16_TF_INT_DIFF",   MDPP16_TF_INT_DIFF,   mdpp16.tf_int_diff);
     set_wr("MDPP16_GAIN",          MDPP16_GAIN,          mdpp16.gain );
@@ -318,14 +319,14 @@ int mdpp16_config(int hVme, unsigned mdpp16_addr)
     set_wr("MDPP16_RISE_TIME",     MDPP16_RISE_TIME,     mdpp16.rise_time );
 
 
-    set_wr("MDPP16_PULSER_CONTROL",MDPP16_PULSER_CONTROL,  mdpp16.pulser_ctrl);
-    set_wr("MDPP16_PULSER_AMP",    MDPP16_PULSER_AMPLITUDE,mdpp16.pulser_amp);
+    set_wr("MDPP16_PULSER_CONTROL", MDPP16_PULSER_CONTROL,  mdpp16.pulser_ctrl);
+    set_wr("MDPP16_PULSER_AMP",    MDPP16_PULSER_AMPLITUDE, mdpp16.pulser_amp);
     set_wr("MDPP16_MARK_TYPE",     MDPP16_MARK_TYPE,       mdpp16.mark_type );
     set_wr("MDPP16_TRIG_OUTPUT",   MDPP16_TRIG_OUTPUT,     mdpp16.trig_output);
-    set_wr("MDPP16_TS_SOURCE",     MDPP16_TS_SOURCE,       mdpp16.ts_source );
-    set_wr("MDPP16_TRIG_OUTPUT",   MDPP16_TRIG_OUTPUT,     mdpp16.trig_output);
+    //set_wr("MDPP16_TS_SOURCE",     MDPP16_TS_SOURCE,       mdpp16.ts_source );
+    //set_wr("MDPP16_TRIG_OUTPUT",   MDPP16_TRIG_OUTPUT,     mdpp16.trig_output);
 
     printf("config_mdpp16: done\n");
-    return(0);
+    return (0);
 }
 
